33bb2690725a9caec3c3b460b929b3ab01b99ed0
CDerksen <christian.derksen@uni-due.de>
2018-01-26 10:29:59 +0100

    Managed to use life cycle manager

--- eclipseProjects/de.enflex.web/bundles/de.enflexit.web.core/src/de/enflexit/web/core/E4LifeCycleManager.java
+++ eclipseProjects/de.enflex.web/bundles/de.enflexit.web.core/src/de/enflexit/web/core/E4LifeCycleManager.java
@@ -1,8 +1,18 @@
 package de.enflexit.web.core;
 
+import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class E4LifeCycleManager {
 
-	
+	@PostContextCreate
+	void postContextCreate(Display display) {
+		
+		final Shell shell = new Shell(SWT.SHELL_TRIM);
+		System.err.println("[" + this.getClass().getName() + "] => It works!");
+		
+	}
 
 }
